a2d83be287dbf8416549bd949cf946828cee3ec6
Pyeroh <quentin.nanteuil@gmail.com>
2015-05-29 16:21:30 +1100

    Premier commit

---
+++ Pyeroh-Crafting-Guide/src/view/Launch.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.io.IOException;
+
+public class Launch {
+
+	public static void main(String[] args) throws IOException {
+		// File mcItems = new
+		// File("C:\\Users\\DENANTEUILQ\\Documents\\WS-eclipse-perso\\Pyeroh-Crafting-Guide\\src\\gui\\items\\mc");
+		// File[] files = mcItems.listFiles();
+		//
+		// for (File file : files) {
+		// File[] icons = file.listFiles();
+		// int i = 0;
+		// if (icons.length > 1) {
+		// if (!icons[i].getName().contains("icon")) {
+		// i++;
+		// }
+		// }
+		// Files.move(
+		// icons[i].toPath(),
+		// Paths.get("C:\\Users\\DENANTEUILQ\\Documents\\WS-eclipse-perso\\Pyeroh-Crafting-Guide\\src\\gui\\items\\mc",
+		// file.getName() + ".png"), StandardCopyOption.ATOMIC_MOVE);
+		// }
+
+	}
+
+}
