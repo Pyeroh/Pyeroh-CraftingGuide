f0d219efc28edd1142a634d08f81fa3b08dd9300
Pyeroh <quentin.nanteuil@gmail.com>
2015-06-16 17:31:26 +1100

    Construction des listes d'ingrédients

--- Pyeroh-Crafting-Guide/src/view/MenuPrincipal.java
+++ Pyeroh-Crafting-Guide/src/view/MenuPrincipal.java
@@ -41,8 +41,8 @@ import view.components.cells.CellListCaracs;
 import view.components.core.JComboSearchField;
 import view.components.core.MCImage;
 import view.components.craft.CellEditQuantity;
-import view.components.craft.FullQuantityItemPanel;
 import view.components.craft.CellEditQuantity.ButtonType;
+import view.components.craft.FullQuantityItemPanel;
 import view.components.event.SearchedItemChangeListener;
 import view.components.infos.ItemDialog;
 
@@ -53,6 +53,8 @@ import view.components.infos.ItemDialog;
  *
  */
 public class MenuPrincipal extends JFrame {
+
+	private static final Font MINECRAFTIA = Launch.getMinecraftia().deriveFont(12f);
 
 	/**
 	 *
@@ -87,11 +89,15 @@ public class MenuPrincipal extends JFrame {
 
 	private JScrollPane scrpan_itemsToMake;
 
-	private FullQuantityItemPanel fqip_itemsToMake;
-
 	private JComboSearchField search_itemsToMake;
 
 	private JPanel pan_ingredients;
+
+	private JScrollPane scrpan_steps;
+
+	private JScrollPane scrpan_ingredientsPresent;
+
+	private JComboSearchField search_ingredientsPresent;
 
 	public MenuPrincipal() {
 		super();
@@ -100,7 +106,7 @@ public class MenuPrincipal extends JFrame {
 		setTitle("Pyeroh Crafting Guide v1");
 
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		setSize(785, 600);
+		setSize(818, 700);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(null);
 
@@ -142,7 +148,7 @@ public class MenuPrincipal extends JFrame {
 		};
 
 		tabpan_mainContainer = new JTabbedPane(JTabbedPane.TOP);
-		tabpan_mainContainer.setBounds(0, 6, 780, 566);
+		tabpan_mainContainer.setBounds(0, 6, 812, 666);
 		getContentPane().add(tabpan_mainContainer);
 
 		pan_configure = new JPanel();
@@ -171,18 +177,18 @@ public class MenuPrincipal extends JFrame {
 		lib_modName = new JLabel(Messages.getString("MenuPrincipal.lib_modName.text")); //$NON-NLS-1$
 		lib_modName.setVisible(false);
 		lib_modName.setFont(Launch.getMinecraftia().deriveFont(14f));
-		lib_modName.setBounds(511, 6, 263, 16);
+		lib_modName.setBounds(543, 6, 263, 16);
 		pan_browse.add(lib_modName);
 
 		lib_modAuthor = new JLabel(); //$NON-NLS-1$
 		lib_modAuthor.setVisible(false);
 		lib_modAuthor.setFont(new Font("SansSerif", Font.ITALIC, 12));
-		lib_modAuthor.setBounds(511, 32, 138, 16);
+		lib_modAuthor.setBounds(543, 32, 138, 16);
 		pan_browse.add(lib_modAuthor);
 
 		scrpan_browsed = new JScrollPane();
 		scrpan_browsed.setVisible(false);
-		scrpan_browsed.setBounds(511, 60, 263, 471);
+		scrpan_browsed.setBounds(543, 60, 263, 495);
 		pan_browse.add(scrpan_browsed);
 
 		tree_browsed = new JXTree();
@@ -239,20 +245,19 @@ public class MenuPrincipal extends JFrame {
 
 		pan_itemsToMake = new JPanel();
 		pan_itemsToMake.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0), 1, true), Messages
-				.getString("MenuPrincipal.pan_itemsToMake.borderTitle"), TitledBorder.LEADING, TitledBorder.TOP, Launch.getMinecraftia()
-				.deriveFont(12f), null));//$NON-NLS-1$
-		pan_itemsToMake.setBounds(6, 6, 768, 228);
+				.getString("MenuPrincipal.pan_itemsToMake.borderTitle"), TitledBorder.LEADING, TitledBorder.TOP, MINECRAFTIA, null)); //$NON-NLS-1$
+		pan_itemsToMake.setBounds(6, 6, 395, 307);
 		pan_craft.add(pan_itemsToMake);
 		pan_itemsToMake.setLayout(null);
 
 		scrpan_itemsToMake = new JScrollPane();
 		scrpan_itemsToMake.setBackground(Color.WHITE);
 		scrpan_itemsToMake.getViewport().setBackground(Color.white);
-		scrpan_itemsToMake.setBounds(6, 25, 756, 151);
+		scrpan_itemsToMake.setBounds(6, 25, 370, 236);
 		scrpan_itemsToMake.getVerticalScrollBar().setUnitIncrement(45);
 		pan_itemsToMake.add(scrpan_itemsToMake);
 
-		fqip_itemsToMake = new FullQuantityItemPanel(ButtonType.ANNULER);
+		final FullQuantityItemPanel fqip_itemsToMake = new FullQuantityItemPanel(ButtonType.ANNULER);
 		scrpan_itemsToMake.setViewportView(fqip_itemsToMake);
 		fqip_itemsToMake.setSize(scrpan_itemsToMake.getViewport().getWidth(), fqip_itemsToMake.getHeight());
 
@@ -274,16 +279,38 @@ public class MenuPrincipal extends JFrame {
 			}
 		});
 		search_itemsToMake.setPrompt(Messages.getString("MenuPrincipal.search_itemsToMake.prompt")); //$NON-NLS-1$
-		search_itemsToMake.setBounds(215, 188, 311, 28);
+		search_itemsToMake.setBounds(6, 273, 370, 28);
 		pan_itemsToMake.add(search_itemsToMake);
 		search_itemsToMake.setColumns(10);
 
 		pan_ingredients = new JPanel();
 		pan_ingredients.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0), 1, true), Messages
-				.getString("FullRecipePanel.lib_ingredients.text"), TitledBorder.LEADING, TitledBorder.TOP, Launch.getMinecraftia()
-				.deriveFont(12f), null));
-		pan_ingredients.setBounds(6, 246, 768, 128);
+				.getString("FullRecipePanel.lib_ingredients.text"), TitledBorder.LEADING, TitledBorder.TOP, MINECRAFTIA, null));
+		pan_ingredients.setBounds(413, 6, 389, 307);
 		pan_craft.add(pan_ingredients);
+		pan_ingredients.setLayout(null);
+
+		scrpan_ingredientsPresent = new JScrollPane();
+		scrpan_ingredientsPresent.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0)), "Already in inventory",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		scrpan_ingredientsPresent.setBounds(6, 25, 377, 111);
+		pan_ingredients.add(scrpan_ingredientsPresent);
+
+		final FullQuantityItemPanel fqip_ingredientsPresent = new FullQuantityItemPanel(ButtonType.RETIRER);
+		scrpan_ingredientsPresent.setViewportView(fqip_ingredientsPresent);
+		fqip_ingredientsPresent.setSize(scrpan_ingredientsPresent.getViewport().getWidth(), fqip_ingredientsPresent.getHeight());
+
+		search_ingredientsPresent = new JComboSearchField();
+		search_ingredientsPresent.setBounds(6, 148, 377, 28);
+		search_ingredientsPresent.setPrompt(Messages.getString("MenuPrincipal.search_itemsToMake.prompt"));
+		pan_ingredients.add(search_ingredientsPresent);
+		search_ingredientsPresent.setColumns(10);
+
+		scrpan_steps = new JScrollPane();
+		scrpan_steps.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0), 1, true), "Steps", TitledBorder.LEADING,
+				TitledBorder.TOP, MINECRAFTIA, null));
+		scrpan_steps.setBounds(6, 325, 796, 305);
+		pan_craft.add(scrpan_steps);
 
 		getContentPane().addMouseListener(new MouseAdapter() {
 
@@ -292,6 +319,7 @@ public class MenuPrincipal extends JFrame {
 				if (e.getSource() instanceof MCImage) {
 					itemDialog.setVisible(false);
 					itemDialog = null;
+					// TODO Gestion du crafting plan
 					System.out.println(((MCImage) e.getSource()).getItem());
 				}
 			}
